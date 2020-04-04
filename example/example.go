247947f51718b72f2b0c56dcfb3648fd71cf7793
cenyang <cenyang@bytedance.com>
2020-04-05 02:38:19 +0800

    update example.go

---
+++ example/example.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yah01/cyflag"
+	"log"
+)
+
+func main() {
+	var (
+		parser cyflag.Parser
+		args   = "i love cyflag -best -times 95"
+
+		best  bool
+		times int
+		love  string
+	)
+	parser.BoolVar(&best, "-best", false, "whether the best")
+	parser.IntVar(&times, "-times", 0, "-times [int]")
+	parser.StringVar(&love, "love", "something", "love [string]")
+
+	if err := parser.ParseString(args);err!=nil {
+		log.Println(err)
+	}
+
+
+	fmt.Printf("best: %+v\n"+
+		"times: %+v\n"+
+		"love: %+v\n",
+		best, times, love)
+}
